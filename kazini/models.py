f2e0fbe098fe195db79a6427e9f19e18a570861b
rowenarono95 <rowenarono@gmail.com>
2020-12-08 21:14:05 +0300

    create user model

--- kazini/models.py
+++ kazini/models.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+ 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    is_employee = models.BooleanField(default=False)
+    is_employer = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=20)
