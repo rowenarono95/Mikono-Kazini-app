e609f9e52771cf7dd0ec1889c30b4a37a6b6cdb7
rowenarono95 <rowenarono@gmail.com>
2020-12-08 23:07:25 +0300

    registered my models

--- kazini/admin.py
+++ kazini/admin.py
@@ -2,7 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import User
+from .models import User,Employee,Employer,Job
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Employee)
+admin.site.register(Employer)
+admin.site.register(Job)
